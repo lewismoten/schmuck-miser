f59c07aebaf1a35543ad9e3d47b84b3c45e5869d
Lewis Moten <lewismoten@gmail.com>
2021-11-01 02:55:00 -0400

    change little-emoji to @lewismoten/emoji

--- src/state/accounts/actions.js
+++ src/state/accounts/actions.js
@@ -1,7 +1,7 @@
 import { createRoutine } from "redux-saga-routines";
-import littleEmoji from "little-emoji";
+import emoji from "@lewismoten/emoji";
 import actionBuilder from "../actionBuilder";
-const build = actionBuilder("accounts", littleEmoji.cardIndexDividers);
+const build = actionBuilder("accounts", emoji.cardIndexDividers);
 
 export const load = build("load");
 export const unload = build("unload");
