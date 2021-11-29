46a8062e7022961b5b92125918d0f2cc2ccdf5b4
Lewis Moten <lewismoten@gmail.com>
2021-11-29 00:00:56 -0500

    add lint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  rules: {
+    quotes: ['error', 'single'],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2021,
+  },
+};
