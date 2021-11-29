d1268d309fa242465e9ef3bfc42780058687ab18
Lewis Moten <lewismoten@gmail.com>
2021-11-29 01:05:20 -0500

    :lipstick: add theming and tie into OS preferred dark

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from './state/accounts/actions';
 import * as selectors from './state/accounts/selectors';
@@ -15,12 +15,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
   const dispatch = useDispatch();
+
   const isLoading = useSelector(selectors.isLoading);
   const hasError = useSelector(selectors.hasError);
   const hasLoaded = useSelector(selectors.hasLoaded);
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const [bottomNavigationValue, setBottomNavigationValue] = useState('a');
 
@@ -33,43 +40,56 @@ function App() {
     return () => dispatch(actions.unload());
   }, []);
 
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <Box className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Home
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Paper elevation={3} variant="outlined">
-        This is an app.
-        <p>HasLoaded: {hasLoaded.toString()}</p>
-        <p>IsLoading: {isLoading.toString()}</p>
-        <p>hasError: {hasError.toString()}</p>
-      </Paper>
-      <BottomNavigation
-        value={bottomNavigationValue}
-        onChange={onChangeBottomNavigation}
-      >
-        <BottomNavigationAction label="Home" value="a" icon={<HomeIcon />} />
-        <BottomNavigationAction
-          label="Settings"
-          value="b"
-          icon={<SettingsIcon />}
-        />
-        <BottomNavigationAction label="I/O" value="c" icon={<SaveIcon />} />
-      </BottomNavigation>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          This is an app.
+          <p>HasLoaded: {hasLoaded.toString()}</p>
+          <p>IsLoading: {isLoading.toString()}</p>
+          <p>hasError: {hasError.toString()}</p>
+        </Paper>
+        <BottomNavigation
+          value={bottomNavigationValue}
+          onChange={onChangeBottomNavigation}
+        >
+          <BottomNavigationAction label="Home" value="a" icon={<HomeIcon />} />
+          <BottomNavigationAction
+            label="Settings"
+            value="b"
+            icon={<SettingsIcon />}
+          />
+          <BottomNavigationAction label="I/O" value="c" icon={<SaveIcon />} />
+        </BottomNavigation>
+      </Box>
+    </ThemeProvider>
   );
 }
 
