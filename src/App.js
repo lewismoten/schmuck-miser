e4ff0ebe4df73b360393cb95f9706565e6e818af
Lewis Moten <lewismoten@gmail.com>
2021-11-29 00:49:32 -0500

    Add app bar to top

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import * as actions from './state/accounts/actions';
 import * as selectors from './state/accounts/selectors';
 
+import AppBar from '@mui/material/AppBar';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
 import SaveIcon from '@mui/icons-material/Save';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
 
 function App() {
   const dispatch = useDispatch();
@@ -28,6 +33,22 @@ function App() {
 
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
       This is an app.
       <p>HasLoaded: {hasLoaded.toString()}</p>
       <p>IsLoading: {isLoading.toString()}</p>
