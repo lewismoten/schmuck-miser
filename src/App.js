4e3c5630fde77341611c568e490e61bdd5dd3871
Lewis Moten <lewismoten@gmail.com>
2021-11-29 00:36:56 -0500

    add bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from './state/accounts/actions';
 import * as selectors from './state/accounts/selectors';
+
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SaveIcon from '@mui/icons-material/Save';
 
 function App() {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectors.isLoading);
   const hasError = useSelector(selectors.hasError);
   const hasLoaded = useSelector(selectors.hasLoaded);
+
+  const [bottomNavigationValue, setBottomNavigationValue] = useState('a');
+
+  const onChangeBottomNavigation = (e, value) => {
+    setBottomNavigationValue(value);
+  };
 
   useEffect(() => {
     dispatch(actions.load());
@@ -20,6 +32,18 @@ function App() {
       <p>HasLoaded: {hasLoaded.toString()}</p>
       <p>IsLoading: {isLoading.toString()}</p>
       <p>hasError: {hasError.toString()}</p>
+      <BottomNavigation
+        value={bottomNavigationValue}
+        onChange={onChangeBottomNavigation}
+      >
+        <BottomNavigationAction label="Home" value="a" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Settings"
+          value="b"
+          icon={<SettingsIcon />}
+        />
+        <BottomNavigationAction label="I/O" value="c" icon={<SaveIcon />} />
+      </BottomNavigation>
     </div>
   );
 }
