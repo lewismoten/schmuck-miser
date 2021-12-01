54355883e0fa75e5b595833d784c6b4de83a42a0
Lewis Moten <lewismoten@gmail.com>
2021-11-30 19:20:09 -0500

    :sparkles: add button to download data

--- src/route/Io/Io.js
+++ src/route/Io/Io.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Page from '../../components/Page';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Button from '@mui/material/Button';
 
-const Io = () => <Page title="I/O">Input / Output</Page>;
+const Io = () => {
+  const onClickDownload = () => {};
+  return (
+    <Page title="I/O">
+      <Button
+        endIcon={<FileDownloadIcon />}
+        variant="outlined"
+        onClick={onClickDownload}
+      >
+        Download
+      </Button>
+    </Page>
+  );
+};
 
 export default Io;
