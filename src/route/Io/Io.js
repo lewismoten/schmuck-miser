5e5986b6dd0dfac9ed67c9573d23fb311ccc7db4
Lewis Moten <lewismoten@gmail.com>
2021-11-30 20:03:17 -0500

    :children_crossing: show downloading indicator

--- src/route/Io/Io.js
+++ src/route/Io/Io.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Page from '../../components/Page';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
 import * as ioActions from '../../state/io/actions';
+import * as ioSelectors from '../../state/io/selectors';
 
 const Io = () => {
   const dispatch = useDispatch();
+  const isDownloading = useSelector(ioSelectors.isDownloading);
 
   const onClickDownload = () => {
     dispatch(ioActions.download());
   };
   return (
     <Page title="I/O">
-      <Button
-        endIcon={<FileDownloadIcon />}
+      <LoadingButton
+        loading={isDownloading}
+        startIcon={<FileDownloadIcon />}
         variant="outlined"
+        loadingPosition="start"
         onClick={onClickDownload}
       >
         Download
-      </Button>
+      </LoadingButton>
     </Page>
   );
 };
