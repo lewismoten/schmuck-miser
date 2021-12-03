d24c60b7013d14e5ef0de8f53ce0979ba597bb7a
Lewis Moten <lewismoten@gmail.com>
2021-12-02 23:41:29 -0500

    :recycle: SoC page bottom navigation

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,28 +1,19 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import { useDispatch } from 'react-redux';
 import * as actions from '../state/accounts/actions';
 
-import { useNavigate, useLocation } from 'react-router-dom';
-
-import AppBar from '@mui/material/AppBar';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import HomeIcon from '@mui/icons-material/Home';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SaveIcon from '@mui/icons-material/Save';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 import PageTitle from './PageTitle';
+import PageNavigation from './PageNavigation';
 
 function Page({ title, children }) {
   const dispatch = useDispatch();
-  let navigate = useNavigate();
-  let location = useLocation();
 
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
@@ -36,15 +27,6 @@ function Page({ title, children }) {
     [prefersDarkMode]
   );
 
-  const [bottomNavigationValue, setBottomNavigationValue] = useState(
-    location.pathname
-  );
-
-  const onChangeBottomNavigation = (e, value) => {
-    setBottomNavigationValue(value);
-    navigate(value);
-  };
-
   useEffect(() => {
     dispatch(actions.load());
     return () => dispatch(actions.unload());
@@ -55,28 +37,7 @@ function Page({ title, children }) {
       <CssBaseline />
       <Box className="App">
         <PageTitle title={title} />
-        <AppBar position="fixed" style={{ top: 'auto', bottom: 0 }}>
-          <BottomNavigation
-            value={bottomNavigationValue}
-            onChange={onChangeBottomNavigation}
-          >
-            <BottomNavigationAction
-              label="Home"
-              value="/"
-              icon={<HomeIcon />}
-            />
-            <BottomNavigationAction
-              label="Settings"
-              value="/settings"
-              icon={<SettingsIcon />}
-            />
-            <BottomNavigationAction
-              label="I/O"
-              value="/io"
-              icon={<SaveIcon />}
-            />
-          </BottomNavigation>
-        </AppBar>
+        <PageNavigation />
         {children}
       </Box>
     </ThemeProvider>
---
+++ src/components/PageNavigation.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import AppBar from '@mui/material/AppBar';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SaveIcon from '@mui/icons-material/Save';
+
+const PageNavigation = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  const [bottomNavigationValue, setBottomNavigationValue] = useState(
+    location.pathname
+  );
+
+  const onChangeBottomNavigation = (e, value) => {
+    setBottomNavigationValue(value);
+    navigate(value);
+  };
+
+  return (
+    <AppBar position="fixed" style={{ top: 'auto', bottom: 0 }}>
+      <BottomNavigation
+        value={bottomNavigationValue}
+        onChange={onChangeBottomNavigation}
+      >
+        <BottomNavigationAction label="Home" value="/" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Settings"
+          value="/settings"
+          icon={<SettingsIcon />}
+        />
+        <BottomNavigationAction label="I/O" value="/io" icon={<SaveIcon />} />
+      </BottomNavigation>
+    </AppBar>
+  );
+};
+
+export default PageNavigation;
