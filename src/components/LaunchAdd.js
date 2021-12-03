3fd343df35ac6b8bd8355d59985ced5efbe3dc01
Lewis Moten <lewismoten@gmail.com>
2021-12-03 00:17:20 -0500

    :construction: floating speed dial in app bar

--- src/components/LaunchAdd.js
+++ src/components/LaunchAdd.js
@@ -6,30 +6,33 @@ import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import AppBar from '@mui/material/AppBar';
 
 const LaunchAdd = () => {
   return (
-    <Box
-      sx={{
-        height: '100%',
-        width: '100%',
-        transform: 'translateZ(0px)',
-        flexGrow: 1,
-      }}
-    >
-      <SpeedDial
-        direction="up"
-        ariaLabel="Add"
-        icon={<SpeedDialIcon />}
-        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+    <AppBar position="fixed" style={{ top: 'auto', bottom: 40 }}>
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%',
+          transform: 'translateZ(0px)',
+          flexGrow: 1,
+        }}
       >
-        <SpeedDialAction
-          icon={<AddBusinessIcon />}
-          tooltipTitle="Business"
-          tooltipOpen
-        />
-      </SpeedDial>
-    </Box>
+        <SpeedDial
+          direction="up"
+          ariaLabel="Add"
+          icon={<SpeedDialIcon />}
+          sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        >
+          <SpeedDialAction
+            icon={<AddBusinessIcon />}
+            tooltipTitle="Business"
+            tooltipOpen
+          />
+        </SpeedDial>
+      </Box>
+    </AppBar>
   );
 };
 
