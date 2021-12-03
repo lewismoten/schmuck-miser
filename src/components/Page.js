bde17c0a3cac347714b5c9ce6cc25ebb803c4dea
Lewis Moten <lewismoten@gmail.com>
2021-12-02 23:45:22 -0500

    :recycle: SoC theme

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,46 +1,21 @@
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import { useDispatch } from 'react-redux';
-import * as actions from '../state/accounts/actions';
-
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
 
+import Theme from './Theme';
 import PageTitle from './PageTitle';
 import PageNavigation from './PageNavigation';
 
 function Page({ title, children }) {
-  const dispatch = useDispatch();
-
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode]
-  );
-
-  useEffect(() => {
-    dispatch(actions.load());
-    return () => dispatch(actions.unload());
-  }, []);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <Theme>
       <Box className="App">
         <PageTitle title={title} />
         <PageNavigation />
         {children}
       </Box>
-    </ThemeProvider>
+    </Theme>
   );
 }
 
---
+++ src/components/Theme.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import { useDispatch } from 'react-redux';
+import * as actions from '../state/accounts/actions';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+const Theme = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  useEffect(() => {
+    dispatch(actions.load());
+    return () => dispatch(actions.unload());
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+Theme.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Theme;
