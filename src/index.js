8a32babbf6467aa3a55a4fb5094d630c2c8bf369
Lewis Moten <lewismoten@gmail.com>
2021-12-04 02:42:21 -0500

    :art: move route components to container pattern

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import Router from './route/Router';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import configureStore from './state/configureStore';
 import ErrorBoundary from './components/ErrorBoundary';
 import RootFallback from './components/RootFallback';
+import Router from './containers/Router';
 
 const { store, persistor } = configureStore();
 
