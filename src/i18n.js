42f498d608f15f988e398511ef1f5abe4b102f21
Lewis Moten <lewismoten@gmail.com>
2021-12-09 01:16:41 -0500

    :globe_with_meridians: Spanish / Español

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-export const languages = ['en', 'en-US', 'de', 'ru'];
+export const languages = ['en', 'en-US', 'de', 'ru', 'es'];
 i18n
   .use(Backend)
   .use(LanguageDetector)
