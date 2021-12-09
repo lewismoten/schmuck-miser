e78171e7060fcd3774bee67af918a9bc4395dfa0
Lewis Moten <lewismoten@gmail.com>
2021-12-09 02:50:31 -0500

    :globe_with_meridians: show language in current and native language

--- src/components/SelectLanguage/SelectLanguage.js
+++ src/components/SelectLanguage/SelectLanguage.js
@@ -4,54 +4,59 @@ import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { useTranslation } from 'react-i18next';
-import emoji from '@lewismoten/emoji';
 
 const options = [
-  { id: 'en-US', label: `${emoji.flagUnitedStates} English (US)` },
-  { id: 'en', label: 'English' },
-  { id: 'ru', label: 'русский' },
-  { id: 'es', label: 'Español' },
-  { id: 'tr', label: 'Türk' },
-  { id: 'fa', label: 'فارسی' },
-  { id: 'fr', label: 'Fransızca' },
-  { id: 'de', label: 'Deutsch' },
-  { id: 'ja', label: '日本' },
-  { id: 'vi', label: 'Tiếng Việt' },
-  { id: 'zh', label: '简体中文' },
-  { id: 'tlh', label: 'tlhIngan Hol' },
+  'en-US',
+  'en',
+  'ru',
+  'es',
+  'tr',
+  'fa',
+  'fr',
+  'de',
+  'ja',
+  'vi',
+  'zh',
+  'tlh',
 ];
-
-const getOptionLabel = (value) => {
-  if (typeof value === 'object' && 'label' in value) return value.label;
-  const option = options.find(({ id }) => id === value);
-  if (option) return option.label;
-  console.log('could not find', { value });
-  return value;
-};
-const renderOption = (props, option) => (
-  <Box component="li" {...props}>
-    {getOptionLabel(option)}
-  </Box>
-);
 
 const SelectLanguage = ({ value, onChange }) => {
   const { t } = useTranslation();
   const __ = (k) => t(`components.selectLanguage.${k}`);
 
-  const handleChange = (e, o, action) => {
-    if (action === 'selectOption') onChange(o.id);
+  const handleChange = (e, option, action) => {
+    if (action === 'selectOption') onChange(option);
   };
 
   const renderInput = ({ inputProps, ...props }) => (
     <TextField
       {...props}
-      label={__`inputLabel`}
+      label={__('inputLabel')}
       inputProps={{ ...inputProps, autoComplete: 'new-password' }}
     />
   );
 
-  const selectedOption = options.find(({ id }) => id === value);
+  const selectedOption = value;
 
+  const getOptionLabel = (option) => {
+    if (option === '') return '';
+    return __(`${option}.label`);
+  };
+
+  const renderOption = (props, option) => {
+    const label = __(`${option}.label`);
+    let native = __(`${option}.native`);
+    if (label === native || native === t('language')) {
+      native = '';
+    }
+
+    return (
+      <Box sx={{ display: 'flex' }} {...props}>
+        <Box sx={{ width: '100%' }}>{label}</Box>
+        <Box sx={{ flexShrink: 0 }}>{native}</Box>
+      </Box>
+    );
+  };
   return (
     <Autocomplete
       value={selectedOption}
