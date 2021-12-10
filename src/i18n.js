03021ece9831efff09dc1536b8ec343bcd6fc15c
Lewis Moten <lewismoten@gmail.com>
2021-12-10 01:31:50 -0500

    :alembic: bundle translations to support local file system hosting
    Local filesystem is doing fetch but fails to load files. Doing dynamic imports allows webpack to chunk it and run further optimizations (decrease file size)

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+// import Backend from 'i18next-http-backend';
+import ChunkedBackend from './i18n.ChunkedBackend';
 
 i18n
-  .use(Backend)
+  // .use(Backend)
+  .use(ChunkedBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
