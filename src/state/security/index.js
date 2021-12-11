188420801c30951e151ad4180b096d53e5d073ab
Lewis Moten <lewismoten@gmail.com>
2021-12-11 18:48:16 -0500

    :lock: store new secret

--- src/state/security/index.js
+++ src/state/security/index.js
@@ -4,15 +4,20 @@ import * as actions from './actions';
 
 const initialState = {
   has2FA: false,
+  tfaNewSecret: undefined,
 };
 
 const onSetup2fa = produce((draft) => {
   draft.has2FA = !draft.has2FA;
 });
+const onSetup2faRequest = produce((draft, action) => {
+  draft.tfaNewSecret = action.payload;
+});
 
 export default handleActions(
   {
     [actions.setup2FA.TRIGGER]: onSetup2fa,
+    [actions.setup2FA.REQUEST]: onSetup2faRequest,
   },
   initialState
 );
