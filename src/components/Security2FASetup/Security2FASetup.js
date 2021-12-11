882ec1baae0645d958575d6115fcfbe2dc459384
Lewis Moten <lewismoten@gmail.com>
2021-12-11 18:14:27 -0500

    :construction: show if 2fa enabled

--- src/components/Security2FASetup/Security2FASetup.js
+++ src/components/Security2FASetup/Security2FASetup.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import * as selectors from '../../state/security/selectors';
 
-const Security2FASetup = () => <div>2fa setup</div>;
+const Security2FASetup = () => {
+  const has2FA = useSelector(selectors.has2FA);
+
+  return <div>2fa setup: {has2FA ? 'Enabled' : 'Disabled'}</div>;
+};
 
 export default Security2FASetup;
