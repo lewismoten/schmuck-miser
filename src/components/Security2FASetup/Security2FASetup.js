f8971fc070d825e4903adff461570b4811dbe4ca
Lewis Moten <lewismoten@gmail.com>
2021-12-11 18:18:20 -0500

    :construction: add button to setup 2FA

--- src/components/Security2FASetup/Security2FASetup.js
+++ src/components/Security2FASetup/Security2FASetup.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import * as selectors from '../../state/security/selectors';
+import * as actions from '../../state/security/actions';
+import Button from '@mui/material/Button';
 
 const Security2FASetup = () => {
+  const dispatch = useDispatch();
   const has2FA = useSelector(selectors.has2FA);
 
-  return <div>2fa setup: {has2FA ? 'Enabled' : 'Disabled'}</div>;
+  const onClick = () => {
+    dispatch(actions.setup2FA());
+  };
+
+  return (
+    <div>
+      <p>2fa setup: {has2FA ? 'Enabled' : 'Disabled'}</p>
+      <Button onClick={onClick}>Setup 2FA</Button>
+    </div>
+  );
 };
 
 export default Security2FASetup;
