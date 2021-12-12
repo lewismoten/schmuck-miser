9956ffc2b53ca08e84f187e58168cafe1c1c1285
Lewis Moten <lewismoten@gmail.com>
2021-12-11 19:03:55 -0500

    :lock: display new secret

--- src/components/Security2FASetup/Security2FASetup.js
+++ src/components/Security2FASetup/Security2FASetup.js
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 const Security2FASetup = () => {
   const dispatch = useDispatch();
   const has2FA = useSelector(selectors.has2FA);
+  const tfaNewSecret = useSelector(selectors.tfaNewSecret);
 
   const onClick = () => {
     dispatch(actions.setup2FA());
@@ -15,6 +16,7 @@ const Security2FASetup = () => {
   return (
     <div>
       <p>2fa setup: {has2FA ? 'Enabled' : 'Disabled'}</p>
+      <p>New Secret: {tfaNewSecret}</p>
       <Button onClick={onClick}>Setup 2FA</Button>
     </div>
   );
--- src/state/security/selectors.js
+++ src/state/security/selectors.js
@@ -3,3 +3,7 @@ import { createSelector } from 'reselect';
 const slice = ({ security = {} } = {}) => security;
 
 export const has2FA = createSelector(slice, ({ has2FA }) => !!has2FA);
+export const tfaNewSecret = createSelector(
+  slice,
+  ({ tfaNewSecret }) => tfaNewSecret
+);
