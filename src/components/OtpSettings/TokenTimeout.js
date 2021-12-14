1b70eaa0db0c81377b999bb89c3c3f5b1a0b4fa7
Lewis Moten <lewismoten@gmail.com>
2021-12-14 01:46:40 -0500

    :iphone: timer alignment

--- src/components/OtpSettings/TokenTimeout.js
+++ src/components/OtpSettings/TokenTimeout.js
@@ -71,12 +71,15 @@ const getDetails = () => {
 };
 
 const sx = {
-  wrapper: { position: 'relative', display: 'inline-flex' },
+  wrapper: {
+    position: 'relative',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   chipWrapper: {
     top: 0,
-    left: 0,
     bottom: 0,
-    right: 0,
     position: 'absolute',
     display: 'flex',
     alignItems: 'center',
