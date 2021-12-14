de3bcf32ccf931db84f3c725fef29ddf418a9fc8
Lewis Moten <lewismoten@gmail.com>
2021-12-14 02:18:48 -0500

    :children_crossing: copy otp secret to keyboard

--- src/components/OtpSettings/CopyButton.js
+++ src/components/OtpSettings/CopyButton.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import copy from 'copy-to-clipboard';
 
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import IconButton from '@mui/material/IconButton';
 
 const CopyButton = ({ text }) => {
-  const onClick = () => {};
+  const onClick = () => {
+    copy(text, {
+      debug: true,
+      format: 'text/plain',
+      message: 'Press #{key} to copy',
+    });
+  };
 
-  return <ContentCopyIcon onClick={onClick} />;
+  return (
+    <IconButton
+      onClick={onClick}
+      color="primary"
+      size="small"
+      aria-label="copy 2FA secret key"
+    >
+      <ContentCopyIcon />
+    </IconButton>
+  );
 };
 
 CopyButton.propTypes = {
