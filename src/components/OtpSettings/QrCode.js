a21dad357c131416e791746350f9b4630e7a0e0b
Lewis Moten <lewismoten@gmail.com>
2021-12-17 22:27:57 -0500

    :lipstick: fallback qr as icon

--- src/components/OtpSettings/QrCode.js
+++ src/components/OtpSettings/QrCode.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import * as selectors from '../../state/otp/selectors';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
+import QrCode2Icon from '@mui/icons-material/QrCode2';
 
 const QRCode = () => {
   const setupImage = useSelector(selectors.setupImage);
@@ -20,7 +21,9 @@ const QRCode = () => {
         variant="rounded"
         src={setupImage}
         sx={{ width: 200, height: 200 }}
-      />
+      >
+        <QrCode2Icon color="error" sx={{ width: 50, height: 50 }} />
+      </Avatar>
     </Box>
   );
 };
