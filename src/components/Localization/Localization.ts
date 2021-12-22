8a16cf583d5a3a69288df731706986ac70db3b50
Lewis Moten <lewismoten@gmail.com>
2021-12-21 21:48:29 -0500

    :rotating_light: localization ts

--- src/components/Localization/Localization.ts
+++ src/components/Localization/Localization.ts
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
-import * as selectors from '../../state/settings/selectors';
+import * as selectors from "../../state/settings/selectors";
 
-const Localization = ({ children }) => {
+interface Props {
+  children: JSX.Element;
+}
+
+const Localization = ({ children }: Props): JSX.Element => {
   const language = useSelector(selectors.language);
   const { i18n } = useTranslation();
   document.body.dir = i18n.dir();
