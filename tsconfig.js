fba59fcf756122c93bc7c84e0e084b08eb79ef80
Lewis Moten <lewismoten@gmail.com>
2021-12-21 19:26:49 -0500

    :wrench: add typescript configuration

---
+++ tsconfig.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  compilerOptions: {
+    outDir: './docs/',
+    noImplicitAny: true,
+    module: 'es6',
+    target: 'es5',
+    jsx: 'react',
+    allowJs: true,
+    moduleResolution: 'node',
+  },
+};
