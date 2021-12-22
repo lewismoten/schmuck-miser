c319b58f9b9cb1048b1b3aba42b98ef46bb82769
Lewis Moten <lewismoten@gmail.com>
2021-12-22 02:26:59 -0500

    :wrench: eslint with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 /* eslint-env node */
 module.exports = {
+  root: true,
   env: {
     es6: true,
     browser: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
     quotes: ['error', 'single'],
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
     sourceType: 'module',
     ecmaVersion: 2021,
   },
   globals: {
     process: true,
   },
+  plugins: ['@typescript-eslint'],
 };
