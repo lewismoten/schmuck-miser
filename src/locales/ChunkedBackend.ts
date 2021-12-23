f46a014cbf77ad32eeb7624b5a91abb0ef1be6de
Lewis Moten <lewismoten@gmail.com>
2021-12-23 01:29:44 -0500

    :rotating_light: :globe_with_meridians: Chunked Backend ts

--- src/locales/ChunkedBackend.ts
+++ src/locales/ChunkedBackend.ts
@@ -1,13 +1,47 @@
-class ChunkedBackend {
-  read(language, namespace, done) {
-    import(/* webpackChunkName: `[request]` */ `./${language}/${namespace}`)
-      .then(({ default: n }) => {
-        done(null, n);
-      })
-      .catch((e) => done(e, false));
-  }
+import { BackendModule, ReadCallback, Services, InitOptions, ResourceKey } from "i18next";
+
+export interface IResourceModule {
+  default: ResourceKey
 }
 
-ChunkedBackend.type = 'backend';
+function isResourceModule(value: any): value is IResourceModule {return typeof value.default === "object";}
 
+const defaultLoader = (language: string, namespace: string): Promise<ResourceKey> => 
+  import(`./${language}/${namespace}`)
+  .then(
+    (resource: IResourceModule | ResourceKey) => 
+    isResourceModule(resource) ? resource.default : resource
+  );
+
+export interface ChunkedOptions {
+  loader? (language: string, namespace: string): Promise<ResourceKey>
+}
+
+class ChunkedBackend implements BackendModule<ChunkedOptions> {
+  constructor(services: Services, chunkedOptions: ChunkedOptions = {}, i18nextOptions: InitOptions = {}) {
+    this.type = 'backend';
+    this.services = services;
+    this.options = chunkedOptions;
+    this.init(services, chunkedOptions, i18nextOptions)
+  }
+  init(services: Services, chunkedOptions: ChunkedOptions, i18nextOptions: InitOptions): void {
+    this.services = services;
+    this.options = chunkedOptions;
+  }
+  read(language: string, namespace: string, done: ReadCallback): void {
+    const load = this.options.loader || defaultLoader;
+    load(language, namespace)
+        .then(resourceKey => done(null, resourceKey))
+        .catch(e => done(e, false));
+  }
+  type:'backend';
+  services: Services;
+  options: ChunkedOptions;
+}
+
+declare module 'i18next' {
+  interface PluginOptions {
+    chunked?: ChunkedOptions
+  }
+}
 export default ChunkedBackend;
--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,22 +1,31 @@
-import i18n from 'i18next';
+import i18n, { InitOptions } from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import ChunkedBackend from './ChunkedBackend';
+import LanguageDetector, { DetectorOptions } from 'i18next-browser-languagedetector';
+import ChunkedBackend, { ChunkedOptions, IResourceModule } from './ChunkedBackend';
+
+const detection: DetectorOptions = {
+  order: ['queryString'],
+  caches: ['localStorage'], // cache or caches? ts wants caches, but was working with cache
+}
+
+const chunked: ChunkedOptions = {
+  loader: (code, ns) => import(/* webpackChunkName: `[request]` */ `./${code}/${ns}`).then((module: IResourceModule) => module.default)
+}
+
+const options: InitOptions = {
+  debug: process.env.NODE_ENV === 'development',
+  fallbackLng: 'en',
+  detection,
+  chunked,
+  interpolation: {
+    escapeValue: false,
+  },
+};
 
 i18n
   .use(ChunkedBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({
-    debug: process.env.NODE_ENV === 'development',
-    fallbackLng: 'en',
-    detection: {
-      order: ['queryString'],
-      cache: ['localStorage'],
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+  .init(options);
 
 export default i18n;
