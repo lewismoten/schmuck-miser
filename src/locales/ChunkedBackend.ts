6ad420a24980ce9a257801459f946d05bc62bca5
Lewis Moten <lewismoten@gmail.com>
2021-12-23 02:05:29 -0500

    :bug: twice as present, chunked backend was awol twice more...

--- src/locales/ChunkedBackend.ts
+++ src/locales/ChunkedBackend.ts
@@ -17,16 +17,26 @@ export interface ChunkedOptions {
   loader? (language: string, namespace: string): Promise<ResourceKey>
 }
 
+const defaultOptions: ChunkedOptions = {
+  loader: defaultLoader
+}
+
 class ChunkedBackend implements BackendModule<ChunkedOptions> {
-  constructor(services: Services, chunkedOptions: ChunkedOptions = {}, i18nextOptions: InitOptions = {}) {
+  static type: 'backend';
+  constructor(services: Services, chunkedOptions: ChunkedOptions = defaultOptions, i18nextOptions: InitOptions = {}) {
     this.type = 'backend';
     this.services = services;
     this.options = chunkedOptions;
     this.init(services, chunkedOptions, i18nextOptions)
   }
-  init(services: Services, chunkedOptions: ChunkedOptions, i18nextOptions: InitOptions): void {
+  init(services: Services, chunkedOptions: ChunkedOptions = defaultOptions, i18nextOptions: InitOptions = {}): void {
+    // Why are chunkedOptions undefined? How do we tell i18next that 'chunked' key is for us?
     this.services = services;
-    this.options = chunkedOptions;
+    this.options = {
+      ...defaultOptions,
+      ...i18nextOptions.chunked || {},
+      ...chunkedOptions
+    };
   }
   read(language: string, namespace: string, done: ReadCallback): void {
     const load = this.options.loader || defaultLoader;
@@ -38,6 +48,8 @@ class ChunkedBackend implements BackendModule<ChunkedOptions> {
   services: Services;
   options: ChunkedOptions;
 }
+
+ChunkedBackend.type = 'backend';
 
 declare module 'i18next' {
   interface PluginOptions {
